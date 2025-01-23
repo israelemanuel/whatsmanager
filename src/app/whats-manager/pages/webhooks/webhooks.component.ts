b4a428f59376c797fe3e683822aed711f99e9a24
israelemanuel97 <israelemanuel97@gmail.com>
2025-01-23 08:37:28 -0300

    updating styles

--- src/app/whats-manager/pages/webhooks/webhooks.component.ts
+++ src/app/whats-manager/pages/webhooks/webhooks.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule }  from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
     selector: 'app-webhooks',
@@ -10,12 +13,16 @@ import { MatRippleModule } from '@angular/material/core';
         CommonModule,
         MatSlideToggleModule,
         MatButtonModule,
-        MatRippleModule
+        MatRippleModule,
+        MatDialogModule,
     ],
     templateUrl: './webhooks.component.html',
     styleUrl: './webhooks.component.scss',
+    encapsulation: ViewEncapsulation.None
 })
-export class WebhooksComponent {
+export class WebhooksComponent implements OnInit {
+
+    public isMobile = false;
 
     public listWebHooks: any = [
         {
@@ -26,8 +33,74 @@ export class WebhooksComponent {
 
     public filteredWeHooks: any = []
 
-    constructor() {
+    constructor(
+        public dialog: MatDialog,
+    ) {
         this.filteredWeHooks = this.listWebHooks
+    }
+
+
+    ngOnInit(): void {
+        this.setViewWidth()
+    }
+
+    addWebHookDialog(): void {
+
+        const dialogRef = this.dialog.open(addWebHookDialog, {
+            panelClass: 'dialog-reset',
+            width: '800px',
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
+
+    setViewWidth() {
+        const width = window.innerWidth;
+
+        if (width < 640) {
+            this.isMobile = true
+        } else {
+            this.isMobile = false
+        }
+
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.setViewWidth()
+    }
+
+}
+
+
+@Component({
+    selector: 'add-webhook-dialog',
+    imports: [
+        CommonModule,
+        MatSelectModule,
+        FormsModule,
+    ],
+    templateUrl: './add-webhook-dialog.component.html',
+    styleUrl: './webhooks.component.scss',
+})
+
+export class addWebHookDialog {
+
+
+    public listTypeHewbHooks: any = [
+        {
+            title: 'Quando receber uma mensagem',
+        },
+    ]
+
+    constructor(
+        public dialogRef: MatDialogRef<addWebHookDialog>,
+    ) { }
+
+    close() {
+        this.dialogRef.close()
     }
 
 }
